9b4ac7538e48132f631aa0e862b13b9b98c9cebd
Sidprog <34118846+Sidprog@users.noreply.github.com>
2022-10-22 21:08:38 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+const port = process.env.PORT || 8000;
+
+//parse request data
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.get('/', (request, respose) => {
+    respose.send('Hello World');
+});
+
+//import employee routes
+const employeeRoutes = require('./src/routes/employee.route');
+
+app.use('/api/v1/employee', employeeRoutes);
+
+app.listen(port, () => {
+    console.log(`Express Server is working at port ${port}`);
+});
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
+Security
